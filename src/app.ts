297911a9647a5f83404c2ede925448461c3c2ad7
João Pedro <joaopedropalomaresjpjp@gmail.com>
2024-02-21 16:03:03 -0300

    Cadastrando e listando as classes: Mamífero e Ave

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { Reptil } from './model/Reptil';
 import { Atracao } from './model/Atracao';
 import { Zoologico } from './model/Zoologico';
 import { DatabaseModel } from './model/DatabaseModel';
+import { Mamifero } from './model/Mamifero';
 
 const port = 3000;
 
@@ -15,7 +16,7 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-    let ave: Ave = new Ave(2.5, "Maria", 1, "Fêmea");
+    let ave: Ave = new Ave("Maria",2.5, "Fêmea" ,1);
     let habitat: Habitat = new Habitat("Selva", [ave]);
     res.json(["A ave é: ", ave, "O habitat é: ", habitat]);
 });
@@ -45,6 +46,60 @@ server.get('/reptil', async (req, res) => {
     const repteis = await Reptil.listarRepteis();
 
     res.status(200).json(repteis);
+})
+
+server.post('/new/reptil', async (req, res) => {
+    const { nome, idade, genero, tipo_de_escamas } = req.body;
+
+    const novoReptil = new Reptil(nome, idade, genero, tipo_de_escamas);
+
+    const result = await Reptil.cadastrarReptil(novoReptil);
+
+    if(result) {
+        return res.status(200).json('Reptil cadastrado com sucesso');
+    } else {
+        return res.status(400).json('Não foi possível cadastrar o réptil no banco de dados');
+    }
+})
+
+server.get('/mamifero', async (req, res) => {
+    const mamiferos = await Mamifero.listarMamiferos();
+
+    res.status(200).json(mamiferos);
+})
+
+server.post('/new/mamifero', async (req, res) => {
+    const { nome, idade, genero, raca } = req.body;
+
+    const novoMamifero = new Mamifero(nome, idade, genero, raca);
+
+    const result = await Mamifero.cadastrarMamifero(novoMamifero);
+
+    if(result) {
+        return res.status(200).json('Mamífero cadastrado com sucesso');
+    } else {
+        return res.status(400).json('Não foi possível cadastrar o mamífero no banco de dados');
+    }
+})
+
+server.get('/ave', async (req, res) => {
+    const aves = await Ave.listarAves();
+
+    res.status(200).json(aves);
+})
+
+server.post('/new/ave', async (req, res) => {
+    const { nome, idade, genero, envergadura } = req.body;
+
+    const novoAve = new Ave(nome, idade, genero, envergadura);
+
+    const result = await Ave.cadastrarAve(novoAve);
+
+    if(result) {
+        return res.status(200).json('Ave cadastrada com sucesso');
+    } else {
+        return res.status(400).json('Não foi possível cadastrar a ave no banco de dados');
+    }
 })
 
 new DatabaseModel().testeConexao().then((resbd) =>{
--- src/model/Reptil.ts
+++ src/model/Reptil.ts
@@ -4,53 +4,52 @@ import { DatabaseModel } from "./DatabaseModel";
 const database = new DatabaseModel().pool;
 
 /**
- * Classe representando um réptil, um tipo de animal de sangue frio que geralmente possui escamas, põe ovos
- * e respira através de pulmões.
+ * Representa um réptil no zoológico, que é uma subclasse de Animal.
  */
-export class Reptil extends Animal{
+export class Reptil extends Animal {
     /**
-     * Representa o tipo de escamas do réptil.
+     * O tipo de escamas do réptil.
      */
-    private tipo_de_escamas: string;
+    private tipo_escamas: string;
 
     /**
-     * Construtor da classe Reptil.
+     * Cria uma nova instância de Reptil.
      * 
-     * @param _tipo_de_escamas O tipo de escamas do réptil.
      * @param _nome O nome do réptil.
      * @param _idade A idade do réptil.
      * @param _genero O gênero do réptil.
+     * @param _tipo_escamas O tipo de escamas do réptil.
      */
-    constructor(_tipo_de_escamas: string, _nome: string, _idade: number, _genero: string){
-        // Chama o construtor da superclasse (classe pai), que é a classe da qual Reptil herda propriedades e métodos.
+    constructor(_nome: string, 
+                _idade: number, 
+                _genero: string, 
+                _tipo_escamas: string) {
         super(_nome, _idade, _genero);
-        // Atribui o tipo de escamas fornecido ao atributo 'tipo_de_escamas' específico da classe Reptil.
-        this.tipo_de_escamas = _tipo_de_escamas;
+        this.tipo_escamas = _tipo_escamas;
     }
 
     /**
-     * Retorna o tipo de escamas do animal
+     * Obtém o tipo de escamas do réptil.
      * 
-     * @returns tipo_de_escamas : tipos de escamas do animal
+     * @returns O tipo de escamas do réptil.
      */
-    public getTipoDeEscamas(): string {
-        return this.tipo_de_escamas;
+    public getTipoEscamas(): string {
+        return this.tipo_escamas;
     }
 
     /**
-     * Atribui o parâmetro ao atributo tipo_de_escamas
+     * Define o tipo de escamas do réptil.
      * 
-     * @param _tipo_de_escamas : tipo de escamas do animal
+     * @param _tipo_escamas O tipo de escamas a ser atribuído ao réptil.
      */
-    public setTipoDeEscamas(_tipo_de_escamas: string): void {
-        this.tipo_de_escamas = _tipo_de_escamas;
+    public setTipoEscamas(_tipo_escamas: string): void {
+        this.tipo_escamas = _tipo_escamas;
     }
 
     static async listarRepteis() {
         const listaDeRepteis: Array<Reptil> = [];
         try {
-            const queryReturn = await database.query(`SELECT * FROM reptil
-            WHERE tipo_de_escamas = 'escudos'`);
+            const queryReturn = await database.query(`SELECT * FROM  reptil`);
             queryReturn.rows.forEach(reptil => {
                 listaDeRepteis.push(reptil);
             });
@@ -63,6 +62,23 @@ export class Reptil extends Animal{
             console.log('Erro no modelo');
             console.log(error);
             return "error";
+        }
+    }
+
+    static async cadastrarReptil(reptil: Reptil): Promise<any> {
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO reptil (nome, idade, genero, tipo_de_escamas)
+                VALUES
+                ('${reptil.getNome().toUpperCase()}', ${reptil.getIdade()}, '${reptil.getGenero().toUpperCase()}', '${reptil.getTipoEscamas().toUpperCase()}');
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            return insertResult;
+        } catch(error) {
+            return error;
         }
     }
 }
